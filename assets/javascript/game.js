d90461bfb0aab23cd8508cf5f6854dfd4813e31d
ravitz <adam.ravitz@gmail.com>
2019-03-09 17:53:32 -0500

    february 9.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -71,60 +71,176 @@ $(document).ready(function() {
         d: "Borg"
       },
       correctAnswer: "Borg"
+    },
+    {
+      Question: "Who created the first warp drive system?",
+
+      Answers: {
+        a: "Zefram Cochrane",
+        b: "Tasha Yar",
+        c: "Lwaxana Troi",
+        d: "Katherine Pulaski"
+      },
+      correctAnswer: "Zefram Cochrane"
+    },  
+    {
+      Question: "In episode 63, Yesterday's Enterprise, who is the captain of the Enterprise C?",
+
+      Answers: {
+        a: "Benjamin Sisko",
+        b: "Rachel Garrett",
+        c: "James Kirk",
+        d: "Tasha Yrr"
+      },
+      correctAnswer: "Rachel Garrett"
+    },
+    {
+      Question: "Darmok and Jalad... at Tanagra",
+
+      Answers: {
+        a: "at Scoomada",
+        b: "at Boobinga",
+        c: "at Tanagra",
+        d: "at Yumgiba"
+      },
+      correctAnswer: "at Tanagra"
+    },
+    {
+      Question: "What kind of pet did Geordi LaForge have when he was 8 years old?",
+
+      Answers: {
+        a: "Tribble",
+        b: "Circassian Cat",
+        c: "Q",
+        d: "Ferangi"
+      },
+      correctAnswer: "Circassian Cat"
+    },
+    {
+      Question: "What was Beverly Crushers rank when she joined the crew?",
+
+      Answers: {
+        a: "Lieutenant",
+        b: "Lieutenant Commander",
+        c: "Commander",
+        d: "Captain"
+      },
+      correctAnswer: "Commander"
     }
   ]
   var wins = 0;
   var losses = 0;
-  var counter = 30;
-  var countDown = false;
   var number = 0; //variable stores the question number 
+  var answers = [];
+        function button() {
 
-        $("#wins").text("Wins: " + wins);
-        $("#losses").text("Losses: " + losses);
+      answers = Object.values(questions[number].Answers);
         $("#questions").text(questions[number].Question);
-    
-    var questionButtonA = $("<button>");
-        questionButtonA
-            .addClass("btn btn-primary btn-answer")
-            .text(questions[number].Answers.a)
-            .attr("data-answer", questions[number].Answers.a);
-        $("#buttons").append(questionButtonA);
 
-        var questionButtonB = $("<button>");
-        questionButtonB
-            .addClass("btn btn-primary btn-answer")
-            .text(questions[number].Answers.b)
-            .attr("data-answer", questions[number].Answers.b);
-        $("#buttons").append(questionButtonB);
+            for (var i = 0; i < answers.length; i++) {
 
-        var questionButtonC = $("<button>");
-        questionButtonC
-            .addClass("btn btn-primary btn-answer")
-            .text(questions[number].Answers.c)
-            .attr("data-answer", questions[number].Answers.c);
-        $("#buttons").append(questionButtonC);
+                var questionButton = $("<button>");
+                    questionButton
+                        .addClass("btn btn-primary btn-answer")
+                        .text(answers[i])
+                        .attr("data-answer", answers[i]);
+                    $("#buttons").append(questionButton);
+                    console.log(answers[i]);
+            }
+                    $(".btn-answer").on("click", function() {
+                      var dataAnswer = ($(this).attr("data-answer"));
+              
+                      if (questions[number].correctAnswer === dataAnswer) {
+                              clearInterval(timer);
+                              resetWin();
+                              console.log("ok!")
 
-        var questionButton4 = $("<button>");
-        questionButton4
-            .addClass("btn btn-primary btn-answer")
-            .text(questions[number].Answers.d)
-            .attr("data-answer", questions[number].Answers.d);
-        $("#buttons").append(questionButton4);
+                      } else  {
+                              clearInterval(timer);
+                              resetLose();
+                              console.log("bummer!")
+                      }
+                    
 
-        $(".btn-answer").on("click", function() {
-        var dataAnswer = ($(this).attr("data-answer"));
-        if (questions[number].correctAnswer === dataAnswer) {
-                wins++
-                console.log("ok!")
-        } else {
-                losses++      
+            });
+          
         }
-            number++
-        console.log(wins);
-        console.log(losses);
-        console.log(number);
-        console.log(dataAnswer);
-        console.log(questions[number]);
-    });
+        
+      
+            var startButton = $("<button>")
+            startButton 
+                .addClass("btn btn-primary btn-start")
+                .text("click here to start")
+                .attr("id", "strtBtn")
+                $("#start").append(startButton);
+
+            $(".btn-start").on("click", function(event) {
+              event.preventDefault()
+              $("#strtBtn").css("display", "none");
+              $("#losses").text("Losses: " + losses);
+              $("#wins").text("Wins: " + wins);
+              button()
+              clock = 31;
+              runClock()
+            }); 
+            console.log(number)
+            
+        function resetWin () {
+            $("#buttons").empty();
+            $("#display").empty();
+            clearInterval(timer);
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            number++;
+            console.log(number);
+            clock = 31;
+            button()
+            runClock()
+        }
+        function resetLose () {
+            $("#buttons").empty();
+            $("#display").empty();
+            clearInterval(timer);
+            losses++
+            $("#losses").text("Losses: " + losses);
+            number++;
+            console.log(number);
+            clock = 31;
+            button()
+            runClock()
+        }
+            
+            var timer;
+            var clock = 31;
+        function countDown() {
+                clock--;
+                $("#display").html(clock);
+                if (clock === 0) {
+                    clearInterval(timer);
+                    resetLose();
+                };
+        };
+        function runClock() {
+            timer = setInterval(countDown, 1000);
+        }; 
+        
+
+        
+
 
 });
+
+/*
+
+.remove() - completly removes everything 
+
+one function called buttons. this function will generate the buttons, 
+append the question to the dom and be reloaded with a new question each time 
+the round ends
+
+one function called start. this will hold the onclick event for starting the game,
+begin the count down timer and take care of the scoring
+
+one function called reset. this will be called at the end of each round, 
+it will raise the var number by 1, which will trigger a new question and set of answers, 
+it will push score information to the DOM, and restart the timer */
